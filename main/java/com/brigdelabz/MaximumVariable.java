bb49783ed266608a8b4831f4eaa418c0a2705836
Pranali0908 <pranali.lembhe77@gmail.com>
2020-01-02 17:09:04 +0530

    Refactored : methods for Integer, Float and String to one Generic method

--- main/java/com/brigdelabz/MaximumVariable.java
+++ main/java/com/brigdelabz/MaximumVariable.java
@@ -1,8 +1,9 @@
 package com.brigdelabz;
 
 public class MaximumVariable {
-    public Integer maximumInteger(Integer x,Integer y, Integer z) {
-        Integer maxValue;
+
+    public static <E extends Comparable> E maximumVariable(E x, E y, E z){
+        E maxValue;
         if (x.compareTo(y)>0){
             maxValue=x;
         }
@@ -17,38 +18,4 @@ public class MaximumVariable {
         return maxValue;
     }
 
-
-    public Float maximumFloat(Float x, Float y, Float z) {
-        Float maxValue;
-        if (x.compareTo(y)>0){
-            maxValue=x;
-        }
-        else {
-            maxValue=y;
-        }
-        if (maxValue.compareTo(z)<0)
-        {
-            maxValue=z;
-        }
-
-        return maxValue;
-
-    }
-
-    public String maximumString(String x, String y, String z) {
-        String maxValue;
-        if (x.compareTo(y)>0){
-            maxValue=x;
-        }
-        else {
-            maxValue=y;
-        }
-        if (maxValue.compareTo(z)<0)
-        {
-            maxValue=z;
-        }
-
-        return maxValue;
-
-    }
 }
--- test/java/com/brigdelabz/MaximumVariableTest.java
+++ test/java/com/brigdelabz/MaximumVariableTest.java
@@ -7,63 +7,63 @@ public class MaximumVariableTest {
     @Test
     public void whenGivenThreeIntegers_shouldReturnFirstVariableMaximum(){
         MaximumVariable maximumVariable = new MaximumVariable();
-        int firstNumber=maximumVariable.maximumInteger(23,12,21);
+        int firstNumber=maximumVariable.maximumVariable(23,12,21);
         Assert.assertEquals(23,firstNumber);
     }
 
     @Test
     public void whenGivenThreeIntegers_shouldReturnSecondVariableMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
-        int secondNumber=maximumVariable.maximumInteger(21,23,21);
+        int secondNumber=maximumVariable.maximumVariable(21,23,21);
         Assert.assertEquals(23,secondNumber);
     }
 
     @Test
     public void whenGivenThreeIntegers_shouldReturnThirdVariableMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
-        int thirdNumber=maximumVariable.maximumInteger(2,12,21);
+        int thirdNumber=maximumVariable.maximumVariable(2,12,21);
         Assert.assertEquals(21,thirdNumber);
     }
 
     @Test
     public void whenGivenThreeFloat_shouldReturnFirstValueMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
-        float firstNumber=maximumVariable.maximumFloat(27.1f,12.4f,21.5f);
+        float firstNumber=maximumVariable.maximumVariable(27.1f,12.4f,21.5f);
         Assert.assertEquals(27.1f,firstNumber,0.0);
     }
 
     @Test
     public void whenGivenThreeFloat_shouldReturnSecondValueMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
-        float firstNumber=maximumVariable.maximumFloat(2.1f,29.4f,21.5f);
+        float firstNumber=maximumVariable.maximumVariable(2.1f,29.4f,21.5f);
         Assert.assertEquals(29.4f,firstNumber,0.0);
     }
 
     @Test
     public void whenGivenThreeFloat_shouldReturnThirdVariableMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
-        float firstNumber=maximumVariable.maximumFloat(2.1f,12.4f,21.5f);
+        float firstNumber=maximumVariable.maximumVariable(2.1f,12.4f,21.5f);
         Assert.assertEquals(21.5f,firstNumber,0.0);
     }
 
     @Test
     public void whenGivenThreeString_shouldReturnFirstStringMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
-        String firstString=maximumVariable.maximumString("Peach","Apple","Banana");
+        String firstString=maximumVariable.maximumVariable("Peach","Apple","Banana");
         Assert.assertEquals("Peach",firstString);
     }
 
     @Test
     public void whenGivenThreeString_shouldReturnSecondStringMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
-        String secondString=maximumVariable.maximumString("Apple","Peach","Banana");
+        String secondString=maximumVariable.maximumVariable("Apple","Peach","Banana");
         Assert.assertEquals("Peach",secondString);
     }
 
     @Test
     public void whenGivenThreeString_shouldReturnThirdStringMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
-        String thirdString=maximumVariable.maximumString("Apple","Banana","Peach");
+        String thirdString=maximumVariable.maximumVariable("Apple","Banana","Peach");
         Assert.assertEquals("Peach",thirdString);
     }
 }
