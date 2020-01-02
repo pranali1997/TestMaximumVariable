4a3231fb3999337f7add1f10a791bb0bab30b47f
Pranali0908 <pranali.lembhe77@gmail.com>
2020-01-02 19:38:53 +0530

    Refactored : internally called printMax method to print values

--- main/java/com/brigdelabz/MaximumVariable.java
+++ main/java/com/brigdelabz/MaximumVariable.java
@@ -17,14 +17,20 @@ public class MaximumVariable <E extends Comparable> {
 
     public E testMaximum() {
         E maximumVar = maximumVariable(x,y,z);
+        printMax((Integer) maximumVar);
         return maximumVar;
     }
 
+    public void printMax(Integer maximumVar)
+    {
+        System.out.println("Value of  "+maximumVar);
+    }
     public static <E extends Comparable> E maximumVariable(E... val){
         E[] values =val;
+        int length=values.length;
         Arrays.sort(values);
 
-        return values[2];
+        return values[length-1];
     }
 
 }
