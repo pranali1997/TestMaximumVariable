67ce17df5419e60395109c890470f3de0d545a7f
Pranali0908 <pranali.lembhe77@gmail.com>
2020-01-02 16:21:17 +0530

    Refactored : changed test methods names

--- main/java/com/brigdelabz/MaximumVariable.java
+++ main/java/com/brigdelabz/MaximumVariable.java
@@ -18,4 +18,5 @@ public class MaximumVariable {
     }
 
 
+
 }
--- test/java/com/brigdelabz/MaximumVariableTest.java
+++ test/java/com/brigdelabz/MaximumVariableTest.java
@@ -5,21 +5,21 @@ import org.junit.Test;
 
 public class MaximumVariableTest {
     @Test
-    public void whenGivenThreeVariables_shouldReturnFirstVariableMaximum(){
+    public void whenGivenThreeIntegers_shouldReturnFirstVariableMaximum(){
         MaximumVariable maximumVariable = new MaximumVariable();
         int firstNumber=maximumVariable.maximumInteger(23,12,21);
         Assert.assertEquals(23,firstNumber);
     }
 
     @Test
-    public void whenGivenThreeVariables_shouldReturnSecondVariableMaximum() {
+    public void whenGivenThreeIntegers_shouldReturnSecondVariableMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
         int secondNumber=maximumVariable.maximumInteger(21,23,21);
         Assert.assertEquals(23,secondNumber);
     }
 
     @Test
-    public void whenGivenThreeVariables_shouldReturnThirdVariableMaximum() {
+    public void whenGivenThreeIntegers_shouldReturnThirdVariableMaximum() {
         MaximumVariable maximumVariable = new MaximumVariable();
         int thirdNumber=maximumVariable.maximumInteger(2,12,21);
         Assert.assertEquals(21,thirdNumber);
